6531771e26555ac1bc181b7bc876a06469af6265
Kraven <kravenkishor.ai@gmail.com>
2025-10-03 03:00:57 +0530

    signup backend files updated

--- models/Users.js
+++ models/Users.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, unique: true, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
