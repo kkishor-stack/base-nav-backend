d5137ec11b8e4b02114b517405b1e540fbd02e24
Kraven <kravenkishor.ai@gmail.com>
2025-11-02 17:05:12 +0530

    updation to delete users utility

--- api/deleteUsers.js
+++ api/deleteUsers.js
@@ -33,5 +33,7 @@ export default async function handler(req, res) {
   } catch (error) {
     console.error("Delete error:", error);
     res.status(500).json({ error: "Server error" });
+  } finally {
+    await mongoose.connection.close().catch(()=> {});
   }
 }
