7882afc4628920a53ace0f059d6d2f3eac326b4a
Kraven <kravenkishor.ai@gmail.com>
2025-11-07 10:37:48 +0530

    username returned

--- api/auth.js
+++ api/auth.js
@@ -45,7 +45,7 @@ export default async function handler(req, res) {
 
             return res.status(200).json({
                 message: 'Login successful!', token: token,
-                user: { id: user._id, email: user.email, }
+                user: { id: user._id, email: user.email, username: user.username },
             });
         }
 
